5ad6b8614ddfff071397b60512c6c41d1bddc9fb
tinkertian <tinker.tian@hotmail.com>
2021-07-13 19:22:35 +0800

    优化代码，减少一次左移运算

--- core/src/main/kotlin/com/tinkertian/snowflake/core/SmallSnowflake.kt
+++ core/src/main/kotlin/com/tinkertian/snowflake/core/SmallSnowflake.kt
@@ -4,47 +4,48 @@ import com.tinkertian.snowflake.api.domain.exception.SnowflakeException
 import org.slf4j.LoggerFactory
 
 open class SmallSnowflake(private var node: Int) {
-    private var logger = LoggerFactory.getLogger(SmallSnowflake::class.java)
-    private val nodeShl = 10
-    private val sequenceShl = 12
-    private val maxNode: Short = 1023
-    private val maxSequence: Short = 4095
-    private var sequence: Short = 0
-    private var referenceTime: Long = 0
+  private var logger = LoggerFactory.getLogger(SmallSnowflake::class.java)
+  private val nodeShl = 10
+  private val sequenceShl = 12
+  private val nodeAndSequenceShl = nodeShl + sequenceShl
+  private val maxNode: Short = 1023
+  private val maxSequence: Short = 4095
+  private var sequence: Short = 0
+  private var referenceTime: Long = 0
 
-    init {
-        if (node < 0 || node > maxNode) {
-            throw IllegalArgumentException(String.format("node must be between %s and %s", 0, maxNode))
-        }
+  init {
+    if (node < 0 || node > maxNode) {
+      throw IllegalArgumentException(String.format("node must be between %s and %s", 0, maxNode))
     }
+  }
 
-    @Synchronized
-    operator fun next(): Long {
-        val currentTime = this.getCurrentTimeMillis()
-        if (currentTime < referenceTime) {
-            throw RuntimeException("Last referenceTime $referenceTime is after reference time $currentTime")
-        } else if (currentTime > referenceTime) {
-            this.sequence = 0
-        } else {
-            if (this.sequence < maxSequence) {
-                this.sequence++
-            } else {
-                throw SnowflakeException("Sequence exhausted at ${this.sequence}, timestamp=${currentTime}")
-            }
-        }
-        referenceTime = currentTime
-
-        val startNano = System.nanoTime()
-        val snowflakeId = currentTime.shl(nodeShl).shl(sequenceShl)
-                .or(node.shl(sequenceShl).toLong())
-                .or(this.sequence.toLong())
-        val endNano = System.nanoTime()
-        logger.debug("It takes time to generate a single snowflake：{} nm", endNano - startNano)
-
-        return snowflakeId
+  @Synchronized
+  operator fun next(): Long {
+    val currentTime = this.getCurrentTimeMillis()
+    if (currentTime < referenceTime) {
+      throw RuntimeException("Last referenceTime $referenceTime is after reference time $currentTime")
+    } else if (currentTime > referenceTime) {
+      this.sequence = 0
+    } else {
+      if (this.sequence < maxSequence) {
+        this.sequence++
+      } else {
+        throw SnowflakeException("Sequence exhausted at ${this.sequence}, timestamp=${currentTime}")
+      }
     }
+    referenceTime = currentTime
 
-    open fun getCurrentTimeMillis(): Long {
-        return System.currentTimeMillis() / 1000
-    }
+    val startNano = System.nanoTime()
+    val snowflakeId = currentTime.shl(nodeAndSequenceShl)
+        .or(node.shl(sequenceShl).toLong())
+        .or(this.sequence.toLong())
+    val endNano = System.nanoTime()
+    logger.debug("It takes time to generate a single snowflake：{} nm", endNano - startNano)
+
+    return snowflakeId
+  }
+
+  open fun getCurrentTimeMillis(): Long {
+    return System.currentTimeMillis() / 1000
+  }
 }
